d05c3b9c00a6681f78c9c1ed736a913aef3ca573
Mehrankhan-METRA-RGB <m.jan9396@hotmail.com>
2022-01-16 21:52:30 +0500

    README.md edited

--- lib/cross_scroll.dart
+++ lib/cross_scroll.dart
@@ -1,3 +1,5 @@
 library cross_scroll;
 
 export 'package:cross_scroll/src/cross_axis_scroll.dart';
+export 'package:cross_scroll/src/cross_scroll_bar.dart';
+export 'package:cross_scroll/src/cross_scroll_decor.dart';
